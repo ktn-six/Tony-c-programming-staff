721c7850adf1c39c3b686bb6f34006bbb86226b7
ktn-six <tk6.ngeno@gmail.com>
2023-02-22 14:15:01 +0300

    learning c

---
+++ scripts/script_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+  char tony,kipruto,ngeno;
+  int age;
+  printf("Enter your initials, and age: ");
+  scanf("%c %c %c %d", &tony, &kipruto, &ngeno, &age);
+printf("My initials are:%c%c%c and my age is %d.\n",tony,kipruto,ngeno,age);
+return(0);
+}
